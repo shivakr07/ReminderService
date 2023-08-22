36f40ae2993d822af5ca7c804a2c8b7e9e1ac25b
shivakr07 <kumarshiva0707@gmail.com>
2023-08-23 03:12:52 +0530

    added notificationticket model

---
+++ src/migrations/20230822212410-create-notification-ticket.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('NotificationTickets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      recepientEmail: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.ENUM,
+        allowNull:false,
+        values : ["PENDING", "SUCCESS", "FAILED"]
+      },
+      notificationTime: {
+        allowNull:false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('NotificationTickets');
+  }
+};
